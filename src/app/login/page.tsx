dc51708e4d8b022ab3bd28c0a97d0de2407f3573
Guru Singh <32260983+guru-singh@users.noreply.github.com>
2026-04-09 09:30:23 +0530

    final build fix - ready for deploy

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 export const dynamic = "force-dynamic";
-export const revalidate = 0;
+//export const revalidate = 0;
 
 import * as React from "react";
 import Link from "next/link";
